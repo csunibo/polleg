1166b5ed4155fd132fca3c245932e4e363a46ab9
faguccio <faga2703@gmail.com>
2024-02-23 17:02:09 +0100

    Feature: creation of Answers
    Fabio tries to rebase
    
    Fabio got last rebase wrong

--- answers/handler.go
+++ answers/handler.go
@@ -1,12 +1,25 @@
 package answers
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 
+	"github.com/csunibo/stackunibo/documents"
 	"github.com/csunibo/stackunibo/util"
 	"github.com/kataras/muxie"
+	"gorm.io/gorm"
 )
+
+type AnswerObj struct {
+	Document  string `json:"document"`
+	Question  uint   `json:"question"`
+	Parent    uint   `json:"parent"`
+	User      string `json:"user"`
+	Content   string `json:"content"`
+	Upvotes   uint32 `json:"upvotes"`
+	Downvotes uint32 `json:"downvotes"`
+}
 
 func AnswerHandler(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
@@ -20,6 +33,27 @@ func AnswerHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func handlePut(res http.ResponseWriter, req *http.Request) {
+	// Declare a new Person struct.
+	var ans AnswerObj
+
+	err := json.NewDecoder(req.Body).Decode(&ans)
+	if err != nil {
+		util.WriteError(res, http.StatusBadRequest, "specify a redirect_uri url param")
+		return
+	}
+
+	// Do something with the Person struct...
+
+	util.Get().Create(&Answer{
+		Document: documents.Document{ID: ans.Document},
+		Question: documents.Question{Model: gorm.Model{ID: ans.Question}},
+
+		Parent:    nil,
+		User:      ans.User,
+		Content:   ans.Content,
+		Upvotes:   ans.Upvotes,
+		Downvotes: ans.Downvotes,
+	})
 }
 
 func handleGet(res http.ResponseWriter, req *http.Request) {
--- util/db.go
+++ util/db.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 
+	"github.com/csunibo/stackunibo/documents"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,9 +16,21 @@ func ConnectDb(ConnStr string) error {
 	if err != nil {
 		return fmt.Errorf("failed to open db connection: %w", err)
 	}
+	TestInit()
 	return nil
 }
 
 func GetDb() *gorm.DB {
 	return db
+}
+
+func TestInit() {
+	db.Create(&documents.Document{
+		ID: "stringasha",
+	})
+
+	db.Create(&documents.Question{
+		Model:    gorm.Model{ID: 123456},
+		Document: documents.Document{ID: "stringasha"},
+	})
 }
