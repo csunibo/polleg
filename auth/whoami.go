c77260e2b2f2813b45db6726aa5d3169d4700972
Luca <luca@teapot.ovh>
2024-02-24 18:45:12 +0100

    remove dead code

--- auth/whoami.go
+++ auth/whoami.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -10,8 +9,6 @@ import (
 
 func WhoAmIHandler(res http.ResponseWriter, req *http.Request) {
 	user := GetUser(req)
-	token, _ := req.Context().Value("token").(string)
-	fmt.Println(token)
 	if err := util.WriteJson(res, user); err != nil {
 		_ = util.WriteError(res, http.StatusInternalServerError, "")
 		slog.Error("could not encode json:", "error", err)
