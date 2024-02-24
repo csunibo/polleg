fbea35457a141fba8621119cd34843edda48574f
Luca <luca@teapot.ovh>
2024-02-24 15:02:25 +0100

    properly case json fields

--- api/answer.go
+++ api/answer.go
@@ -91,7 +91,7 @@ func GetQuestionHandler(res http.ResponseWriter, req *http.Request) {
 	}
 
 	var question Question
-	if err := db.First(&question, uint(qID)).Error; err != nil {
+	if err := db.Preload("Answers").Preload("Answers.Replies").First(&question, uint(qID)).Error; err != nil {
 		util.WriteError(res, http.StatusInternalServerError, "Question not found")
 		return
 	}
